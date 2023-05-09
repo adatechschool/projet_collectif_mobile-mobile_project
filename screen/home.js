0fe25ca6bdba14e52d8b368e6b5008bf752d1258
SandyDambreville <114993056+SandyDambreville@users.noreply.github.com>
2023-05-09 12:49:38 +0200

    ongoing parsing json

--- screen/home.js
+++ screen/home.js
@@ -1,40 +1,36 @@
 import { StatusBar } from 'expo-status-bar';
 import { Text, View, ScrollView, TouchableOpacity, StyleSheet } from 'react-native';
+import data from './details.json';
+import {useEffect} from 'react';
 
 export default function Home({ navigation }) {
-  const spots = [{
-    name: "Gold Coast",
-    image: "https://images.unsplash.com/photo-1476574898132-040f50db0a01?ixlib=rb-4.0.3&ixid=MnwxM[…]90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
-    location: "Melbourne, Australia"
-  }, {
-    name: "Reef Break",
-    image: "https://images.unsplash.com/photo-1459745930869-b3d0d72c3cbb?ixlib=rb-4.0.3&ixid=MnwxM[…]90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80'",
-    location: "Hawai, United States"
-  },{
-    name: 'Beach Break',
-    image: 'https://images.unsplash.com/photo-1482531007909-192ac913980a?ixlib=rb-4.0.3&ixid=MnwxM[…]90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1171&q=80',
-    location: 'New York, United States'
-  }
-]
+
+  useEffect(()=>{
+    try {
+      const response = fetch('./details.json');
+      const json = response.json();
+      console.log(json);
+    } catch (error) {
+      console.error(error);
+    }
+  },[]
+  )
   return (
     <ScrollView>
       <StatusBar style="auto" />
       <View style={styles.container}>
-        {spots.map(data => {
-        return (
-          <>
-            <TouchableOpacity 
-            onPress={() => {
-                navigation.navigate('Details', {
-                    name: data.name,
-                    image: data.image,
-                    location: data.location            })
+        {data.map(data => {
+          return (
+            <>
+              <TouchableOpacity
+                onPress={() => {
+                  navigation.navigate('Details')
                 }}
-            >
-              <Text style={styles.text}>{data.name}</Text>
-            </TouchableOpacity>
-          </>
-        )
+              >
+                <Text style={styles.text}>{data.name}</Text>
+              </TouchableOpacity>
+            </>
+          )
         })}
       </View>
     </ScrollView>
@@ -42,13 +38,13 @@ export default function Home({ navigation }) {
 }
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      marginTop: 50,
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-    text: {
-      fontSize: 25
-    }
-  });
+  container: {
+    flex: 1,
+    marginTop: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 25
+  }
+});
