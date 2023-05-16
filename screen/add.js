e5a74040a7de7d60cc4578f84d24ae274bd28f5b
abj95630 <abah20000@gmail.com>
2023-05-16 15:53:00 +0200

    images uploader in add spot view added

--- screen/add.js
+++ screen/add.js
@@ -1,12 +1,27 @@
-import { View, ScrollView, StyleSheet, Button, TextInput, SafeAreaView } from 'react-native';
-import { useEffect, useState } from 'react';
+import { View, StyleSheet, Button, TextInput, SafeAreaView, Image } from 'react-native';
+import {useState } from 'react';
+import * as ImagePicker from 'expo-image-picker';
 
 export default function Add() {
     const [name, setName] = useState('');
     const [location, setLocation] = useState('');
+    const [imageUri, setImageUri] = useState(null);
+
+    const pickImage = async () => {
+        let result = await ImagePicker.launchImageLibraryAsync({
+            mediaTypes: ImagePicker.MediaTypeOptions.All,
+            allowsEditing: true,
+            aspect: [4, 3],
+            quality: 1
+        });
+
+        if(!result.canceled) {
+            setImageUri(result.assets[0].uri);
+        }
+    }
 
     return (
-        <SafeAreaView>
+        <SafeAreaView style={{flex: 1, justifyContent: "center"}}>
             <View style={style.inputs}>
                 <TextInput 
                 style={style.input} 
@@ -20,6 +35,11 @@ export default function Add() {
                 value={location}
                 placeholder='location'
                 />
+                <Button title='Select an image' onPress={pickImage}/>
+                {imageUri && 
+                <Image source={{ uri: imageUri }} 
+                style={{ width: 100, height: 100, marginLeft: "auto", marginRight:"auto" }}
+                />}
             </View>
         </SafeAreaView>
     )
